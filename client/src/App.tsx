9c5629f265227ce654f015f03a29375854f7ef56
Kurler3 <miguelbento2000@gmail.com>
2023-04-05 19:01:31 +0100

    Add reac query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,32 @@
 import "./App.css"
+import axios from "axios";
+import {
+  useQuery
+} from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({});
 
 function App() {
- 
+  
+  const {
+    isLoading,
+    isError,
+    data,
+    error,
+    refetch
+  } = useQuery(
+    ["notes"],
+    () => axios.get("http://localhost:5000/api/notes/list")
+    .then((res) => res.data)
+  )
+
+    if(isLoading) return "Loading...";
+
+    if(error) return "An error has occurred:" + error;
+
+    console.log(data);
+
   return (
     <div className="App">
       
---
+++ client/src/types/note.types.ts
@@ -0,0 +1,8 @@
+
+export type Note = {
+    _id: string;
+    title: string;
+    text: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
